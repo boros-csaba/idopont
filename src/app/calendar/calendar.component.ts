eeed0d88ace1c64966f7b3c3c25da382cfa8a3a3
boroscsaba94 <boros.csaba94@gmail.com>
2020-01-04 23:12:39 +0100

    Clickable fixed calendar

--- src/app/calendar/calendar.component.ts
+++ src/app/calendar/calendar.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-calendar',
   templateUrl: 'calendar.component.html',
   styleUrls: ['calendar.component.css']
 })
-export class CalendarComponent {
+export class CalendarComponent implements OnInit {
 
-  
+  year = 2020;
+  month = "Január";
+  daysInMonth: number[][];
+  currentDay = 7;
+  selectedDay = 23;
+
+  constructor() {
+    this.daysInMonth = [[null, null, 1, 2, 3, 4, 5],[6, 7, 8, 9, 10, 11, 12], [13, 14, 15, 16, 17, 18, 19], [20, 21, 22, 23, 24, 25, 26], [27, 28, 29, 30, 31, null, null]];
+  }
+
+  ngOnInit() {
+    
+  }
+
+  onDaySelected(day: number) {
+    this.selectedDay = day;
+  }
 
 }
